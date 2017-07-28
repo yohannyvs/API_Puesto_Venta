c876bc1d2195438a67eadaed61574302a4771661
yohannyvs <yohannyvs@gmail.com>
2017-07-28 13:37:32 -0600

    act 1
    get categorias funcionando

--- API_Puesto_Venta/API_Puesto_Venta/Controllers/PruebaController.cs
+++ API_Puesto_Venta/API_Puesto_Venta/Controllers/PruebaController.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using API_Puesto_Venta.Models;
 
 namespace API_Puesto_Venta.Controllers
 {
     public class PruebaController : ApiController
     {
+        DataClassesDataContext dc = new DataClassesDataContext();
         // GET: api/Prueba
+        
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<string> list = new List<string>();
+
+            var result = dc.select_categoria();
+
+            foreach (var res in result)
+            {
+                list.Add(res.nombre_cat);
+            }
+
+            return list;
         }
 
         // GET: api/Prueba/5
